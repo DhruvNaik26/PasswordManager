fb251786acc3b983789cc162b7729da3e90106d4
DhruvNaik26 <ndhruv26@gmail.com>
2024-07-13 22:03:39 +0530

    No new significant changes were dne here

--- GUI.py
+++ GUI.py
@@ -2,6 +2,7 @@
 from tkinter import *  # type: ignore # Import everything from tkinter 
 import customtkinter as ck # Import customtkinter for enhanced tkinter widgets
 
+
 # Set the appearance mode and default color theme
 ck.set_appearance_mode("dark")
 ck.set_default_color_theme("green")
@@ -12,11 +13,16 @@ win.title("Password Manager")
 
 # Set the size of the window
 win.geometry("600x300")
+def sth():
+    
+    lab1.configure(text="Hellowguys ")
 
 
-
-but1=ck.CTkButton(win,text="Close the application", command=win.destroy)
+but1=ck.CTkButton(win,text="Close the application", command=sth, corner_radius=25,font=("Buttermilk",15),height=50,width=30)
 but1.pack(side=LEFT)
+
+lab1=ck.CTkLabel(win,text="")
+lab1.pack(side=LEFT)
 
 # Start the main loop to display the window
 win.mainloop()
